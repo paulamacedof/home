0cff629d20551cd4d0580b2428aa4e0b3a1c78a2
paulamacedof <paulamacedof@outlook.com>
2025-03-13 16:47:15 -0300

    chore: added state example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+function App({ state, actions }) {
+  debugger;
+  const changeTitle = () => {
+    actions.setMicrofrontendTitle("Home");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">Microfrontend HOME</header>
+      <header className="App-header">
+        {state?.microfrontendTitle}
+        <button onClick={changeTitle}>mudar titulo</button>
+      </header>
     </div>
   );
 }

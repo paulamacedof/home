5f7116601d5105f28ebbcf98e21cc55cb67ad343
Guilherme <guilherme.bail@gauge.haus>
2025-03-13 17:24:13 -0300

    setting store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+function App({ state, actions }: any) {
+  const handleClick = () => {
+    actions.setState("Hello home");
+  };
+
+  console.log("Estado atualizado:", state);
   return (
     <div className="bg-black h-screen flex justify-center items-center">
       <header className="text-white text-7xl">Microfrontend HOME</header>
+      <p>home state: {state}</p>
+      <button onClick={handleClick}>mudar titulo</button>
     </div>
   );
 }
